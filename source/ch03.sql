795da60c51b66bc2c8c83fa2c0a319fa26af6226
geoblo <geoblo@naver.com>
2025-12-11 16:19:24 +0900

    up

--- source/ch03.sql
+++ source/ch03.sql
@@ -279,16 +279,6 @@ WHERE math > 80 AND programming > 80 OR English > 90;
 -- ④ 이 쿼리를 실행하면 그 결과로 컬럼 3개가 조회된다.
 -- ⑤ SELECT 절의 '(math + english + programming) / 3' 수식에 괄호를 삭제하고 조회해도 정상적으로 중간고사 평균이 계산된다.
 
--- 정답: 
-
-
-
-
-
-
-
-
-
-
+-- 정답: 2, 5
 
 
