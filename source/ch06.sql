1e9ac9e11b4301e7a074e7c8e48a762acfc39e0c
geoblo <geoblo@naver.com>
2025-12-15 12:37:26 +0900

    up

--- source/ch06.sql
+++ source/ch06.sql
@@ -306,8 +306,6 @@ VALUES
 SELECT * FROM teams;
 SELECT * FROM players;
 
-
-
 -- 3. 다대다 관계 만들기
 -- A와 B 테이블 서로가 다수의 데이터와 연결되는 관계
 -- - A 테이블의 한 데이터가 B 테이블의 여러 데이터와 연결
@@ -315,11 +313,61 @@ SELECT * FROM players;
 -- 중간 테이블을 경유하여 A와 B가 연결됨
 -- 다대다 관계에서는 중간 테이블을 만들어 외래키 지정
 
+-- doctors 테이블 생성
+CREATE TABLE doctors (
+	id INT, 
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
 
+-- patients 테이블 생성
+CREATE TABLE patients (
+	id INT, 
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
 
+-- appointments 테이블 생성
+CREATE TABLE appointments (
+	id INT, 
+    doctor_id INT, -- 의사 아이디
+    patient_id INT, -- 환자 아이디
+    date DATE, -- 진료 일자
+    PRIMARY KEY (id),
+    FOREIGN KEY (doctor_id) REFERENCES doctors(id), -- 외래키 지정: doctor_id
+    FOREIGN KEY (patient_id) REFERENCES patients(id) -- 외래키 지정: patient_id
+);
 
+DESC appointments;
 
+-- doctors 데이터 삽입
+INSERT INTO doctors (id, name)
+VALUES
+	(1, '김 닥터'),
+	(2, '이 닥터'),
+	(3, '최 닥터');
 
+-- patients 데이터 삽입
+INSERT INTO patients (id, name)
+VALUES
+	(1, '환자 A'),
+	(2, '환자 B'),
+	(3, '환자 C');
+
+-- appointments 데이터 삽입
+INSERT INTO appointments (id, doctor_id, patient_id, date)
+VALUES
+	(1, 1, 1, '2025-01-01'),
+	(2, 1, 2, '2025-01-02'),
+	(3, 2, 2, '2025-01-03'),
+	(4, 2, 3, '2025-01-04'),
+	(5, 3, 3, '2025-01-05'),
+	(6, 3, 1, '2025-01-06');
+
+-- 확인
+SELECT * FROM doctors;
+SELECT * FROM patients;
+SELECT * FROM appointments;
 
 
 
