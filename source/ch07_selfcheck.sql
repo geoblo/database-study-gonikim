7f3358dcab9fc096bfb9b8da1bf3e7dbe8ec3eeb
geoblo <geoblo@naver.com>
2025-12-16 14:48:43 +0900

    up

--- source/ch07_selfcheck.sql
+++ source/ch07_selfcheck.sql
@@ -6,14 +6,35 @@
 -- ------------------------
 -- 닉네임      | 계정 공개 여부
 -- ------------------------
+SELECT 
+	nickname AS 닉네임,
+    private AS '계정 공개 여부',
+    -- 추가 문법: CASE문
+    CASE
+		WHEN private = 0 THEN '비공개'
+        ELSE '공개'
+	END AS '계정 공개 여부2'
+FROM users u
+JOIN settings s ON u.id = s.user_id;
+
 -- 2. 누가 올렸는지 확인할 수 있는 사진에 대해서만 사진 파일명과 올린 사람을 다음과 같이 조회하세요.
 -- ------------------------
 -- 파일명      | 게시자
 -- ------------------------
+SELECT 
+	filename AS 파일명,
+    nickname AS 게시자
+FROM photos p
+JOIN users u ON p.user_id = u.id;
+
 -- 3. 모든 사진에 대해 사진 파일명과 올린 사람을 다음과 같이 조회하세요.(올린 사람이 누구인지 모르는 사진도 조회합니다.)
 -- ------------------------
 -- 파일명      | 게시자
 -- ------------------------
-
+SELECT 
+	filename AS 파일명,
+    nickname AS 게시자
+FROM photos p
+LEFT JOIN users u ON p.user_id = u.id;
 
 
