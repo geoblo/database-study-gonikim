652dd7ac481aec5e52e5655f14a65556436e6fb8
geoblo <geoblo@naver.com>
2025-12-18 11:58:46 +0900

    up

--- source/ch09_selfcheck.sql
+++ source/ch09_selfcheck.sql
@@ -17,7 +17,10 @@ FROM users;
 
 -- 정답: 
 -- 전체 사용자의 1인당 평균 결제 금액: SELECT 절의 서브쿼리
-
+-- 평균 결제 금액 = 전체 결제 금액 / 전체 사용자 수
+SELECT 
+	SUM(amount) / (SELECT COUNT(*) FROM users) AS '1인당 평균 결제 금액'
+FROM payments;
 
 
 -- 2. 다음은 사용자 아이디별로 총 결제 금액을 집계하는 쿼리입니다.
@@ -35,7 +38,15 @@ GROUP BY u.id;
 
 -- 정답:
 -- 전체 사용자의 1인당 평균 결제 금액: FROM 절의 서브쿼리
-
+SELECT 
+	AVG(sub.total_amount) AS '1인당 평균 결제 금액'
+FROM (
+	SELECT u.id AS user_id, SUM(amount) AS total_amount
+	FROM users u
+	JOIN orders o ON u.id = o.user_id
+	JOIN payments p ON o.id = p.order_id
+	GROUP BY u.id
+) AS sub; -- FROM 절 서브쿼리 별칭 필수
 
 
 -- 3. 앞의 두 문제(1과 2)의 정답 쿼리를 실행하면 다음과 같이 동일한 결과가 나옵니다. 그 이유를 설명하세요.
@@ -44,8 +55,12 @@ GROUP BY u.id;
 -- ------------------
 -- 41790.0000
 
--- 정답: 
+-- 정답: 모든 사용자가 결제에 참여했기 때문
+-- 1은 결제에 참여하지 않은 사람 포함 전체 인원으로 집계, 2는 진짜 결제한 사람만 집계
 
+-- 테스트
+INSERT INTO users (email, nickname)
+VALUES ('geoblo@naver.com', '김재');
 
 
 -- 4. 다음은 최근에 배송받은 사용자의 아이디를 찾는 쿼리입니다.
@@ -58,5 +73,19 @@ LIMIT 1;
 
 -- 정답:
 -- 최근에 배송받은 사용자(특정 사용자)의 총 결제 금액
+SELECT 
+	u.nickname,
+	SUM(amount)
+FROM users u
+JOIN orders o ON u.id = o.user_id
+JOIN payments p ON o.id = p.order_id
+WHERE u.id = (
+	SELECT user_id
+	FROM orders
+	WHERE status = '배송 완료'
+	ORDER BY created_at DESC
+	LIMIT 1
+)
+GROUP BY u.nickname;
 
 
